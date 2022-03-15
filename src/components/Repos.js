5bcacd6ded1812a16790db920ef82ea6070b5cfc
Priyanka <original.manaria@gmail.com>
2022-03-15 14:31:14 +0530

    repos setup

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
 
 const Repos = () => {
+  const { repos } = useContext(GithubContext);
+  console.log(repos);
   return <div>Repos</div>;
 };
+
+const Wrapper = styled.div`
+  display: grid;
+  justify-items: center;
+  gap: 2rem;
+  @media (min-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (min-width: 1200px) {
+    grid-template-columns: 2fr 3fr;
+  }
+  div {
+    width: 100% !important;
+  }
+  .fusioncharts-container {
+    width: 100% !important;
+  }
+  svg {
+    width: 100% !important;
+    border-radius: var(--radius) !important;
+  }
+`;
 
 export default Repos;
